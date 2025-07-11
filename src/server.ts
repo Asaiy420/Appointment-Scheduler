0e7047b83ece067498ac17371e44306f0d87439f
Asaiy420 <kcaryan05@gmail.com>
2025-07-11 17:58:19 +0545

    feat: add user routes for handling user-related API endpoints

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,9 @@
 import express from "express";
 import "dotenv/config";
 import cors from "cors";
-import authRoutes from "./routes/auth.routes";
 import cookieParser from "cookie-parser";
+import authRoutes from "./routes/auth.routes";
+import userRoutes from "./routes/user.routes";
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -13,6 +14,7 @@ app.use(cookieParser());
 
 
 app.use("/api/auth", authRoutes);
+app.use("/api/user", userRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
