37628023f54e6e3fd3648d2d63e93e3bb9783169
Asaiy420 <kcaryan05@gmail.com>
2025-07-12 21:43:45 +0545

    feat: refactor doctor controller to implement getDoctorById functionality

--- src/controllers/doctor.controller.ts
+++ src/controllers/doctor.controller.ts
@@ -1,8 +1,53 @@
 import { Request, Response } from "express";
+import prisma from "../config/db";
 
+export const getAllDoctors = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  try {
+    const doctors = await prisma.doctor.findMany();
 
+    res.status(200).json({
+      message: "Doctors fetched successfully",
+      data: doctors,
+    });
+    return;
+  } catch (e) {
+    console.error("Error in getAllDoctos controller", e);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
 
+export const getDoctorById = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  const { id } = req.params;
 
-export const getAllDoctors = async (req:Request, res: Response): Promise<void> => {
-    
-}
+  try {
+    if (!id) {
+      res.status(404).json({ error: "ID not found" });
+      return;
+    }
+
+    const doctor = await prisma.doctor.findUnique({
+      where: {
+        id: id,
+      },
+    });
+
+    if (!doctor) {
+      res.status(404).json({ error: "Doctor not found" });
+      return;
+    }
+
+    res.status(200).json({
+      message: "Doctor retrieved successfully",
+      data: doctor,
+    });
+  } catch (e) {
+    console.error("Error in getDoctorById controller", e);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
