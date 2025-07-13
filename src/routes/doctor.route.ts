7b6ae848a84b0fe09d1a1b148773f84e0a10963d
Asaiy420 <kcaryan05@gmail.com>
2025-07-13 13:22:10 +0545

    feat: add updateDoctor route for updating doctor details

--- src/routes/doctor.route.ts
+++ src/routes/doctor.route.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import { doctorRoute } from "../middleware/auth.middleware";
-import { getAllDoctors, getDoctorById } from "../controllers/doctor.controller";
+import { getAllDoctors, getDoctorById, updateDoctor } from "../controllers/doctor.controller";
 
 const router = Router();
 
 router.get("/", doctorRoute, getAllDoctors)
 router.get("/:id", doctorRoute, getDoctorById)
+router.put("/:id", doctorRoute, updateDoctor) 
 
 export default router;
